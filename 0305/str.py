1b9b1bb9d2bdf7b876ad82e254ddb8f58126efae
Zac <a19860101@gmail.com>
2026-03-05 19:23:26 +0800

    20260305

--- 0305/str.py
+++ 0305/str.py
@@ -4,18 +4,25 @@ b = "hello"
 
 print(a)
 
+# 字串切片
 # 變數[索引值]
 # print(a[2])
 # print(a[0])
 # print(a[-1])
 
 s = '0123456789'
-print(s[0:4])
-print(s[2:6])
-print(s[-5:-1])
-print(s[3:-2])
-print(s[0:3])
-print(s[:3])
-print(s[5:])
-print(s[:])
+# print(s[0:4])
+# print(s[2:6])
+# print(s[-5:-1])
+# print(s[3:-2])
+# print(s[0:3])
+# print(s[:3])
+# print(s[5:])
+# print(s[:])
+
+# print(s[0:9:5])
+# print(s[::2])
+# print(s[1::2])
+# print(s[::-1])
+# print(s[::-2])
 
