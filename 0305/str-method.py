077e8a03e13b7fe716d83c693391db2f2c63800c
Zac <a19860101@gmail.com>
2026-03-05 20:24:36 +0800

    20260305

--- 0305/str-method.py
+++ 0305/str-method.py
@@ -3,20 +3,35 @@
 s = 'hElLo World World'
 # 字串大小寫
 # 每個單字的首字大寫
-print(s.title())
+# print(s.title())
 # 轉大寫
-print(s.upper())
+# print(s.upper())
 # 轉小寫
-print(s.lower())
+# print(s.lower())
 # 首字大寫
-print(s.capitalize())
+# print(s.capitalize())
 
 #
+# find()
+# 尋找。若有找到回傳該文字存在的索引值
 # print(s.find('H'))
 # print(s.find('E'))
-print(s.find('o'))
+# print(s.find('o'))
 # print(s.find('World'))
-print(s.rfind('o'))
+# 由右側尋找。若有找到回傳該文字存在的索引值
+# print(s.rfind('o'))
 
-print(s.replace('World', 'python'))
-print(s.replace('world', 'python'))
+# replace()
+# 取代文字
+# print(s.replace('World', 'python'))
+# print(s.replace('world', 'python'))
+
+
+# 計算
+# count()
+print(s.count('o'))
+print(s.count('a'))
+print(s.count('World'))
+
+# len()
+print(len(s))
