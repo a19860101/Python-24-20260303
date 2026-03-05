3fa258b3f2d1bef2264246f63aa42575939d9a4e
Zac <a19860101@gmail.com>
2026-03-05 19:10:23 +0800

    20260305

--- 0305/str.py
+++ 0305/str.py
@@ -5,6 +5,17 @@ b = "hello"
 print(a)
 
 # 變數[索引值]
-print(a[2])
-print(a[0])
-print(a[-1])
+# print(a[2])
+# print(a[0])
+# print(a[-1])
+
+s = '0123456789'
+print(s[0:4])
+print(s[2:6])
+print(s[-5:-1])
+print(s[3:-2])
+print(s[0:3])
+print(s[:3])
+print(s[5:])
+print(s[:])
+
