3b8e09e3752a0d6194d244ce4ed652c1aa6ab458
Zac <a19860101@gmail.com>
2026-03-05 21:28:43 +0800

    20260305

--- 0305/f-string.py
+++ 0305/f-string.py
@@ -1,8 +1,22 @@
 # f-string
 
 name = 'John'
-n = 65
+n = 25
 
 # print('hello ' + name)
-print(f'Hello {name}! 您是第{n}位訪客')
-print('Hello '+name+'! 您是第' + str(n) +'位訪客')
+# print(f'Hello {name}! 您是第{n}位訪客')
+# print('Hello '+name+'! 您是第' + str(n) +'位訪客')
+
+pi = 3.1415926
+# 指定小數位數
+print(f'{pi:.2f}')
+
+s = 0.3687
+# 轉換百分比
+print(f'{s:.2%}')
+
+money = 29600
+print(f'{money:,}')
+
+idn = 6
+print(f'{idn:05}')
