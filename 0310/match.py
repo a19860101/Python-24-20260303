2feeae4357abd62535478e35da9352c10a1a1221
Zac <a19860101@gmail.com>
2026-03-10 21:10:13 +0800

    20260310

---
+++ 0310/match.py
@@ -0,0 +1,27 @@
+# match
+
+day = 89
+
+# if day == 0:
+#     print('星期日')
+# elif day == 1:
+#     print('星期一')
+
+# 3.10
+match day:
+    case 0:
+        print('星期日')
+    case 1:
+        print('星期一')
+    case 2:
+        print('星期二')
+    case 3:
+        print('星期三')
+    case 4:
+        print('星期四')
+    case 5:
+        print('星期五')
+    case 6:
+        print('星期六')
+    case _:
+        print('系統錯誤')
