a95557bd57ece123aab5bb0f89bd71869b317afc
Zac <a19860101@gmail.com>
2026-03-12 19:24:15 +0800

    20260312

---
+++ 0312/list.py
@@ -0,0 +1,16 @@
+l1 = ['apple', 'banana', 'cat','dog','elephant','fork']
+# print(l1)
+# print(l1[0])
+# print(l1[1])
+# print(l1[2])
+# list切片
+# list[start: end: step]
+# print(l1[0:3])
+# print(l1[:4])
+# print(l1[4:])
+# print(l1[::-1])
+# print(l1[::2])
+
+# 遍歷
+for x in l1:
+    print(x)
---
+++ 0312/loop.py
@@ -0,0 +1,16 @@
+# loop
+
+# for
+# rang(start, end, step)
+# 確定次數時使用
+# for i in range(100,0,-2):
+#     print(i)
+
+
+# while
+# 不確定次數時使用
+x = 0
+while x < 10:
+    # x = x + 1
+    x += 1
+    print(x)
