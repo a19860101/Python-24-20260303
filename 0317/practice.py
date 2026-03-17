7f3e0029f8a571ce2418d716cb9d6d6d92cec88a
Zac <a19860101@gmail.com>
2026-03-17 21:10:03 +0800

    20260317

--- 0317/practice.py
+++ 0317/practice.py
@@ -2,16 +2,27 @@ import requests
 
 url = 'https://itunes.apple.com/search'
 
-my_params = {
-    'term':'周杰倫',
-    'entity':'album',
-    'country':'TW'
-}
+while True:
 
-res = requests.get(url, params=my_params)
+    term = input('請輸入關鍵字或輸入q結束程式:')
 
-data = res.json()
+    if term == 'q':
 
-for item in data['results']:
-    # print(item)
-    print(f'專輯名稱:{item.get('collectionName')} | 歌手:{item.get('artistName')} | 發行日期:{item.get('releaseDate')[:10]}')
+        print('BYE!')
+        break
+
+    my_params = {
+        'term':term,
+        'entity':'album',
+        'country':'TW'
+    }
+
+    res = requests.get(url, params=my_params)
+
+    data = res.json()
+    print(data)
+    for item in data['results']:
+        print(item)
+        print(f'專輯名稱:{item.get('collectionName')} | 歌手:{item.get('artistName')} | 發行日期:{item.get('releaseDate')[:10]}')
+
+    print('*' * 100)
