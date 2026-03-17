59e1fa96faf94f7f726cc18acbededd0d6106350
Zac <a19860101@gmail.com>
2026-03-17 19:02:39 +0800

    20260317

--- 0317/dict.py
+++ 0317/dict.py
@@ -6,7 +6,10 @@ user = {
     'mail': 'john@gmail.com'
 }
 
-print(user)
-print(user['name'])
-print(user['age'])
-print(user['mail'])
+# print(user)
+# print(user['name'])
+# print(user['age'])
+# print(user['mail'])
+
+print(user.get('name', '找不到資料'))
+print(user.get('hobby', '找不到資料'))
