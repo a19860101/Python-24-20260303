7492f3eae3e9cabc478e3c247d116f4b3e6a8b1c
Zac <a19860101@gmail.com>
2026-03-17 20:47:34 +0800

    20260317

--- 0317/practice.py
+++ 0317/practice.py
@@ -4,10 +4,14 @@ url = 'https://itunes.apple.com/search'
 
 my_params = {
     'term':'周杰倫',
+    'entity':'album',
     'country':'TW'
 }
 
 res = requests.get(url, params=my_params)
 
-print(res.json())
+data = res.json()
 
+for item in data['results']:
+    # print(item)
+    print(f'專輯名稱:{item.get('collectionName')} | 歌手:{item.get('artistName')} | 發行日期:{item.get('releaseDate')[:10]}')
