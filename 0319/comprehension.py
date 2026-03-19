893d9f26c5f848f61984826e9094c9fd5b427142
Zac <a19860101@gmail.com>
2026-03-19 21:12:40 +0800

    20260319

--- 0319/comprehension.py
+++ 0319/comprehension.py
@@ -1,6 +1,7 @@
 # 生成式
 
 list1 = [1,2,3,4,5,6,7,8,9]
+drinks = ['紅茶','綠茶','美式咖啡','拿鐵']
 
 # 原始寫法
 # result = []
@@ -13,7 +14,13 @@ list1 = [1,2,3,4,5,6,7,8,9]
 # result = [data ** 2 for data in list1 ]
 # result = [str(data) for data in list1 ]
 # result = [float(data) for data in list1 ]
-result = [ data+3.1415926 for data in list1 ]
+# result = [ data+3.1415926 for data in list1 ]
 
+# result = [data for data in list1 if data > 5]
+# result_even = [data for data in list1 if data % 2 == 0]
+# result_odd = [data for data in list1 if data % 2 == 1]
+
+# result = [data for data in drinks if data.find('茶')>-1]
+result = [data for data in drinks if data.find('茶')==-1]
 
 print(result)
