a32e883af2fa27894d3fb8b827e3ef290b861305
Zac <a19860101@gmail.com>
2026-03-19 18:54:22 +0800

    20260319

---
+++ 0319/tuple.py
@@ -0,0 +1,18 @@
+# tuple
+
+# 建立
+t1 = ('apple', 'banana', 'cherry')
+
+# print(t1)
+# print(type(t1))
+
+# t2 = tuple([1,2,3,4])
+# print(t2)
+
+# t3 = ()
+# t4 = tuple()
+# print(type(t4))
+
+t5 = (1,)
+print(t5)
+print(type(t5))
