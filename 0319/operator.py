3ad0b7b06bd127e1a5cd394b937e1790728519a1
Zac <a19860101@gmail.com>
2026-03-19 21:40:59 +0800

    20260319

---
+++ 0319/operator.py
@@ -0,0 +1,27 @@
+a = 2
+b = 9
+
+# 算術
+print(a + b)
+print(a - b)
+print(a * b)
+print(a ** 2)
+print(a ** 0.5)
+print(a / b)
+print(a % b)
+
+
+# 比較
+print(a > b)
+print(a >= b)
+print(a < b)
+print(a <= b)
+print(a == b)
+
+# 指定
+# a = b
+# a += b => a = a + b
+# a -= b => a = a - b
+# a *= b => a = a * b
+# a /= b => a = a / b
+# a %= b => a = a % b
