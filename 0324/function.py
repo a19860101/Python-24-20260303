432469c2d8417f4ff83798d841ebff34c8f57998
Zac <a19860101@gmail.com>
2026-03-24 19:21:21 +0800

    20260324

---
+++ 0324/function.py
@@ -0,0 +1,15 @@
+def tw_us(dollar, rate):
+    print(dollar / rate)
+
+def us_tw(dollar, rate=32):
+    return dollar * rate
+
+# result = us_tw(1799, 32)
+
+# print(result)
+
+# print(us_tw(1799, 32))
+
+# print(us_tw(rate=31,dollar=1799))
+
+print(us_tw(1799,30))
