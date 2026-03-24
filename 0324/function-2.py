c3164644cf10854dd6592a4aedc0bfdf37aa2a79
Zac <a19860101@gmail.com>
2026-03-24 19:45:28 +0800

    20260324

--- 0324/function-2.py
+++ 0324/function-2.py
@@ -10,7 +10,18 @@
 
 # test(95,90,80,65,88,15,57)
 
-def test(**attribute):
-    print(attribute)
+# def test(**attribute):
+    # print(attribute)
 
-test(name='john',mail='asdf@gmail.com',age=18)
+# test(name='john',mail='asdf@gmail.com',age=18)
+
+# def avg(*scores):
+    # print(sum(scores))
+    # print(len(scores))
+    # print(sum(scores) / len(scores))
+
+# avg(100,80,30,90,85,65,50)
+def aa(*price,people):
+    print(sum(price) / people)
+
+aa(5,180,80,30,300,60,210, people=10)
