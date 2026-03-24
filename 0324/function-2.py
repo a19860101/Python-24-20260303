c74667c49307adc31460a318e8f87bc7fc69a77c
Zac <a19860101@gmail.com>
2026-03-24 19:32:12 +0800

    20260324

---
+++ 0324/function-2.py
@@ -0,0 +1,16 @@
+# def test(*args):
+#     # print(args)
+#     for arg in args:
+#         print(arg)
+
+# test('apple','banana','cat')
+
+# def test(*score):
+    # print(score)
+
+# test(95,90,80,65,88,15,57)
+
+def test(**attribute):
+    print(attribute)
+
+test(name='john',mail='asdf@gmail.com',age=18)
