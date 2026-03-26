a55bf07c43e33459b1d23de7637fb0b82f13770c
Zac <a19860101@gmail.com>
2026-03-26 20:54:44 +0800

    20260326

--- 0326/fs_csv.py
+++ 0326/fs_csv.py
@@ -5,8 +5,14 @@ data = [
     ['apple2','banana2','mango2','peach2','pineapple2']
 ]
 
-with open('0326.csv','w',encoding='utf-8') as f:
+with open('0326.csv','w',encoding='utf-8', newline='') as f:
     writer = csv.writer(f)
     writer.writerows(data)
     # writer.writerow(data)
     # csv.writer(f).writerow(data)
+
+with open('0326.csv','r',encoding='utf-8') as f:
+    reader = csv.reader(f)
+    # print(reader)
+    for row in reader:
+        print(row)
