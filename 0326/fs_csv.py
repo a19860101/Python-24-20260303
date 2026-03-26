67cc289ce9129efd237ee7ebe30ec43e03931ec3
Zac <a19860101@gmail.com>
2026-03-26 20:44:10 +0800

    20260326

---
+++ 0326/fs_csv.py
@@ -0,0 +1,12 @@
+import csv
+
+data = [
+    ['apple','banana','mango','peach','pineapple'],
+    ['apple2','banana2','mango2','peach2','pineapple2']
+]
+
+with open('0326.csv','w',encoding='utf-8') as f:
+    # writer = csv.writer(f)
+    # writer.writerows(data)
+    # writer.writerow(data)
+    csv.writer(f).writerow(data)
