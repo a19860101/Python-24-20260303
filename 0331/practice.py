92362e68caa97c214bb551c6832ff04862c3bb8f
Zac <a19860101@gmail.com>
2026-03-31 21:03:14 +0800

    20260331

--- 0331/practice.py
+++ 0331/practice.py
@@ -1,11 +1,22 @@
 import os
+import shutil
 
-target_dir = '00000'
+target_dir = './00000'
+
+new_dir = 'html'
+
+if not os.path.exists(new_dir):
+    os.makedirs(new_dir)
 
 all_files = os.listdir(target_dir)
+
 
 for item in all_files:
     # print(os.path.basename(item))
     # print(os.path.splitext(item)[1][1:])
     name, ext = os.path.splitext(item)
-    print(ext)
+
+    old_path = os.path.join(target_dir,item)
+    new_path = os.path.join(new_dir,item)
+    if ext == '.html':
+        shutil.move(old_path, new_path)
