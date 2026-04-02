09faf5f98d52b79d02ccfbf68ce0ac04f96a9663
Zac <a19860101@gmail.com>
2026-04-02 21:42:58 +0800

    20260402

--- 0402/pillow-4.py
+++ 0402/pillow-4.py
@@ -7,25 +7,20 @@ print(images)
 for image in images:
     img_name = os.path.basename(image)
     img = Image.open(image)
+    w1, h1 = img.size
+    w2 = 200
+    h2 = int((h1 * w2) / w1)
+    s_img = img.resize((w2,h2))
     os.makedirs('./thumbs', exist_ok=True)
-    img.save(f'./thumbs/s_{img_name}', quality=1)
+    s_img.save(f'./thumbs/s_{img_name}', quality=1)
 
-for i,image in enumerate(images):
-    name,ext = os.path.splitext(image)
-    img = Image.open(image)
-    os.makedirs('./thumbs', exist_ok=True)
-    img.save(f'./thumbs/{i+1}{ext}', quality=1)
+# for i,image in enumerate(images):
+#     name,ext = os.path.splitext(image)
+#     img = Image.open(image)
+#     w1, h1 = img.size
+#     w2 = 200
+#     h2 = int((h1 * w2) / w1)
+#     s_img = img.resize((w2,h2))
+#     os.makedirs('./thumbs', exist_ok=True)
+#     s_img.save(f'./thumbs/{i+1}{ext}', quality=1)
 
-# # img = Image.open('./images/shana-van-roosbroek-gdPqNWGfqW4-unsplash.jpg')
-#
-# w1, h1 = img.size
-#
-# w2 = int(input('請輸入縮圖的寬：'))
-#
-# h2 = int((h1 * w2) / w1)
-#
-# small_img = img.resize((w2,h2))
-#
-
-#
-# small_img.save('thumbs/test.jpg', quality=75)
