090ecb39d4fb443275426549eae533c812e33db7
Zac <a19860101@gmail.com>
2026-04-07 19:53:04 +0800

    20260407

---
+++ 0407/pillow-6.py
@@ -0,0 +1,29 @@
+from PIL import Image, ImageEnhance
+import glob
+
+images = glob.glob('./images/*.[jJ][pP][gG]')
+
+img = Image.open(images[0])
+
+# contrast = ImageEnhance.Contrast(img)
+# img = contrast.enhance(0.1)
+
+# brightness = ImageEnhance.Brightness(img)
+# img = brightness.enhance(0.3)
+
+# color = ImageEnhance.Color(img)
+# img = color.enhance(0)
+
+sharpness = ImageEnhance.Sharpness(img)
+img1 = sharpness.enhance(0.1)
+img2 = sharpness.enhance(5)
+img1.save('img1.jpg')
+img2.save('img2.jpg')
+
+# img_1 = ImageEnhance.Contrast(img).enhance(0.2)
+# img_2 = ImageEnhance.Brightness(img_1).enhance(3)
+# img_3 = ImageEnhance.Color(img_2).enhance(0)
+
+# img_3.show()
+
+
