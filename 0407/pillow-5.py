d9f9ba22667576e2a5df279f7acbf86677f3723b
Zac <a19860101@gmail.com>
2026-04-07 19:31:59 +0800

    20260407

--- 0407/pillow-5.py
+++ 0407/pillow-5.py
@@ -12,4 +12,12 @@ img = Image.open(images[0])
 # rotated_img = img.rotate(90,expand=True,fillcolor='pink')
 # rotated_img.save('rotated_img.jpg')
 
-transpose_img = img.transpose()
+# transpose_img = img.transpose(Image.FLIP_LEFT_RIGHT)
+# transpose_img = img.transpose(Image.FLIP_TOP_BOTTOM)
+# transpose_img = img.transpose(Image.Transpose.FLIP_LEFT_RIGHT)
+# transpose_img = img.transpose(Image.Transpose.FLIP_TOP_BOTTOM)
+# transpose_img = img.transpose(Image.Transpose.ROTATE_90)
+# transpose_img = img.transpose(Image.Transpose.ROTATE_180)
+transpose_img = img.transpose(Image.Transpose.ROTATE_270)
+transpose_img.save('transpose_img.jpg')
+
