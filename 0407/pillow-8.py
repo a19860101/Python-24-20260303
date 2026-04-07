13fed01dae511fe9e30a79239afd905bb7f02fb2
Zac <a19860101@gmail.com>
2026-04-07 21:21:01 +0800

    20260407

--- 0407/pillow-8.py
+++ 0407/pillow-8.py
@@ -8,7 +8,15 @@ w,h = img.size
 
 draw = ImageDraw.Draw(img)
 font = ImageFont.truetype('./NotoSerifTC-Black.ttf', 200)
-
-t,l,w1,h1 = draw.textbbox((0,0),'我',font=font)
+text = '聯成電腦'
+t,l,w1,h1 = draw.textbbox((0,0),text,font=font)
 
 print(w1,h1)
+draw.text((0,0),text, font=font, fill='pink')
+draw.text((w-w1,h-h1),text, font=font, fill='pink')
+draw.text((w-w1,0),text, font=font, fill='pink')
+draw.text((0,h-h1),text, font=font, fill='pink')
+draw.text((w/2 - w1/2 , h/2-h1/2),text, font=font, fill='pink')
+
+img.save('./output.jpg')
+
