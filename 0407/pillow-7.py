a8668ed5f4e5ebefd01a2d1211138e092d86a5c3
Zac <a19860101@gmail.com>
2026-04-07 20:42:00 +0800

    20260407

--- 0407/pillow-7.py
+++ 0407/pillow-7.py
@@ -11,7 +11,7 @@ draw = ImageDraw.Draw(img)
 font = ImageFont.truetype('./NotoSerifTC-Black.ttf', 200)
 
 # draw.text((w-1200,h-200),"聯電", font_size=200,font=font)
-draw.text((0,0),"聯成電腦",font=font)
+draw.text((0,0),"聯成電腦",font=font, fill="red")
 
 # img.show()
 img.save('./output.jpg')
