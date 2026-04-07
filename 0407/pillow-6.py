d28d74188076d994415e58e29d90df56d10a38a2
Zac <a19860101@gmail.com>
2026-04-07 19:53:54 +0800

    20260407

--- 0407/pillow-6.py
+++ 0407/pillow-6.py
@@ -5,15 +5,19 @@ images = glob.glob('./images/*.[jJ][pP][gG]')
 
 img = Image.open(images[0])
 
+# 對比
 # contrast = ImageEnhance.Contrast(img)
 # img = contrast.enhance(0.1)
 
+# 亮度
 # brightness = ImageEnhance.Brightness(img)
 # img = brightness.enhance(0.3)
 
+# 飽和度
 # color = ImageEnhance.Color(img)
 # img = color.enhance(0)
 
+# 銳利度
 sharpness = ImageEnhance.Sharpness(img)
 img1 = sharpness.enhance(0.1)
 img2 = sharpness.enhance(5)
