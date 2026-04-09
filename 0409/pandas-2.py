d68896b0e0b29cf7f25b7232ae4fcb331a813b91
Zac <a19860101@gmail.com>
2026-04-09 19:31:56 +0800

    20260409

--- 0409/pandas-2.py
+++ 0409/pandas-2.py
@@ -6,10 +6,18 @@ users = [
     {'name':'Kate','birth':'1999','gender':'f'},
     {'name':'Liam','birth':'1998','gender':'m'},
 ]
-df = pd.DataFrame(users)
+df = pd.DataFrame(users,index=['A','B','C','D'],columns=['Name','Birth','Gender'])
 
 print(df)
 # print(df.index)
 # print(df.columns)
 # print(df.shape)
 # print(df.size)
+
+# print(df['name'])
+# print(df['gender'])
+# print(df.loc['A'])
+# print(df.iloc[0])
+# print(df.loc[1])
+
+
