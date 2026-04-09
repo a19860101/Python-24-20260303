3a6027eb4961e50ebf3d9615453d48a4e922e3ba
Zac <a19860101@gmail.com>
2026-04-09 21:32:49 +0800

    20260409

--- 0409/pandas-3.py
+++ 0409/pandas-3.py
@@ -1,5 +1,20 @@
 import pandas as pd
 
 datas = pd.read_json('./每日各站進出站人數-2026.json')
-datas.columns = ['乘車日','車站代碼','進站人數','出戰人數']
-print(datas)
+
+# datas.columns = ['乘車日','車站代碼','進站人數','出戰人數']
+# print(datas['trnOpDate'])
+# 216
+# condition = datas['trnOpDate'] == 20260216
+# print(datas[condition]['gateInComingCnt'].max())
+
+# datas216 = datas[condition] #216進站人數
+
+# datas216_max = datas216['gateInComingCnt'].idxmax()
+
+# print(datas216.loc[datas216_max])
+
+taipei = datas[datas['staCode']==1000]
+taipei_max = taipei['gateInComingCnt'].idxmax()
+print(taipei.loc[taipei_max])
+
