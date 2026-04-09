c4022e301eb043d9d02033b8677483c5d3ef2030
Zac <a19860101@gmail.com>
2026-04-09 20:15:14 +0800

    20260409

--- 0409/pandas-2.py
+++ 0409/pandas-2.py
@@ -6,7 +6,7 @@ users = [
     {'name':'John','birth':'1990','gender':'m'},
     {'name':'Mary','birth':'1997','gender':'f'},
     {'name':'Kate','birth':'1999','gender':'f'},
-    {'name':'Liam','birth':'2022','gender':'m'},
+    {'name':'Liam','birth':'2006','gender':'m'},
 ]
 df = pd.DataFrame(users,index=['A','B','C','D'],columns=['name','birth','gender'])
 # df.columns = ['姓名','出生年份','性別']
@@ -17,10 +17,17 @@ print(df)
 # print(df.size)
 
 current_year = datetime.today().year
-print(current_year)
+# print(current_year)
 
 df['age'] = current_year - df['birth'].astype(int)
-print(df)
+# print(df)
+
+print(df['age'].min())
+print(df['age'].max())
+print(df['age'].mean())
+print(df['age'].median())
+print(df['age'].std())
+print(df['age'].describe())
 
 
 # print(df['name'])
@@ -28,5 +35,4 @@ print(df)
 # print(df.loc['A'])
 # print(df.iloc[0])
 # print(df.loc[1])
-
 
