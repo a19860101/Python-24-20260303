a677adbfdc255a118e2b200b8120271d54874209
Zac <a19860101@gmail.com>
2026-04-14 20:02:19 +0800

    20260414

--- 0414/pandas-5.py
+++ 0414/pandas-5.py
@@ -2,4 +2,14 @@ import pandas as pd
 
 df = pd.read_json('./TransService.json')
 # animal_kind
-print(df['animal_Variety'])
+# print(df['animal_Variety'])
+
+# condition = df['animal_kind'].str.contains('蛇')
+# print(df[condition])
+
+# animal_types = df['animal_kind'].unique()
+# animal_types = df['animal_Variety'].unique()
+# area = df['shelter_name'].unique()
+
+kind_count = df['shelter_name'].value_counts()
+print(kind_count)
