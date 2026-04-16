e2877bff9fec3dbb0ede22162ca15f23689bba5f
Zac <a19860101@gmail.com>
2026-04-16 19:00:23 +0800

    20260416

---
+++ 0416/pandas-8.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+result = pd.read_csv('./Restaurant_C_f.csv')
+
+# print(result.columns)
+# print(result['Name'].isnull())
+# print(result['Name'].isnull().sum())
+# print(result['Add'].isnull().sum())
+# print(result['Region'].isnull().sum())
+
+# condition = result['Region'].isnull()
+
+# empty_region = result[condition]
+
+# print(empty_region)
+
+# print(result.isnull().sum())
