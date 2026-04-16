ccda2dff973724af201550c0743ec2300444fa73
Zac <a19860101@gmail.com>
2026-04-16 19:23:17 +0800

    20260416

--- 0416/pandas-8.py
+++ 0416/pandas-8.py
@@ -2,16 +2,21 @@ import pandas as pd
 
 result = pd.read_csv('./Restaurant_C_f.csv')
 
+print(result)
 # print(result.columns)
 # print(result['Name'].isnull())
 # print(result['Name'].isnull().sum())
 # print(result['Add'].isnull().sum())
 # print(result['Region'].isnull().sum())
 
-# condition = result['Region'].isnull()
-
-# empty_region = result[condition]
-
-# print(empty_region)
+condition = result['Region'].isnull()
+empty_region = result[condition]
+print(empty_region)
 
 # print(result.isnull().sum())
+
+r = result.dropna(axis=1)
+print(r.columns)
+
+r2 = result.dropna(axis=0,subset=['Region'])
+print(r2)
