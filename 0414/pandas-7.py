6766c139824d420d004b82a387b03e762ac89c86
Zac <a19860101@gmail.com>
2026-04-16 18:26:49 +0800

    20260416

--- 0414/pandas-7.py
+++ 0414/pandas-7.py
@@ -1,14 +1,17 @@
 import pandas as pd
 import json
 
-# with open('./restaurant_C_f.json', 'r', encoding='utf-8-sig') as f:
-#     json_data = json.load(f)
-#
-#
-# data = pd.DataFrame(json_data)
-#
+with open('./restaurant_C_f.json', 'r', encoding='utf-8-sig') as f:
+    json_data = json.load(f)
+
+
+data = pd.DataFrame(json_data['XML_Head']['Infos']['Info'])
+
+# print(data.iloc[0])
+# print(data.columns)
+
+condition = data['Add'].str.contains('新北市220板橋區')
+print(data[condition]['Name'])
+
+# data = pd.read_csv('./Restaurant_C_f.csv')
 # print(data)
-
-data = pd.read_csv('./Restaurant_C_f.csv')
-
-print(data)
