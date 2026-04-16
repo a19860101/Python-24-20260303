c082d390df23f06aa9507fc214f9f7f8f61f90e3
Zac <a19860101@gmail.com>
2026-04-16 20:33:55 +0800

    20260416

--- 0416/pandas-youbike.py
+++ 0416/pandas-youbike.py
@@ -5,5 +5,14 @@ import pandas as pd
 # ar 地址
 # sna 站點名稱
 
+result = pd.read_json('./youbike_immediate.json')[['sarea','sna','ar','available_rent_bikes']]
 
+condition = (result['sarea'].str.contains('大安區')) & (result['available_rent_bikes'] > 5)
 
+result = result[condition]
+
+result['sna'] = result['sna'].str.replace('YouBike2.0_', '')
+result.columns = ['行政區','站點名稱','地址','可借數量']
+print(result)
+
+result.to_excel('YouBike2.xlsx')
