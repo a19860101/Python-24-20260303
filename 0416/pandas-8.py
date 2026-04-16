ee235a193ab889156dc63c2bdb29d567d73a16de
Zac <a19860101@gmail.com>
2026-04-16 19:42:32 +0800

    20260416

--- 0416/pandas-8.py
+++ 0416/pandas-8.py
@@ -14,9 +14,19 @@ empty_region = result[condition]
 print(empty_region)
 
 # print(result.isnull().sum())
+#
+# r = result.dropna(axis=1,how='all')
+# print(r.columns)
 
-r = result.dropna(axis=1)
-print(r.columns)
+# r2 = result.dropna(axis=0,subset=['Region'])
+# print(r2)
 
-r2 = result.dropna(axis=0,subset=['Region'])
-print(r2)
+# NaN,None,NaT
+
+# axis：往哪邊砍？（0砍列，1砍欄）
+# subset：砍哪一區？（指定哪幾個欄位）
+# how：砍多兇？（看到一個就砍，還是全空才砍）
+
+fill_region = empty_region.fillna('區域未知')
+
+print(fill_region['Region'])
