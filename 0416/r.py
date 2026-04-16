eba52639931d16d07fc40384813c8b391c7e4b37
Zac <a19860101@gmail.com>
2026-04-16 21:38:55 +0800

    20260416

---
+++ 0416/r.py
@@ -0,0 +1,12 @@
+n = '9527'
+
+# n = list(n)
+# n.reverse()
+# n = ''.join(n)
+# print(n)
+
+# n = reversed(n)
+# n = ''.join(n)
+# print(n)
+
+# print(''.join(reversed(n)))
--- 0416/tri-1.py
+++ 0416/tri-1.py
@@ -9,6 +9,14 @@
 #     print(i * '*')
 # for i in range(5):
 #     print((i+1) * '*')
+n=7
+for i in range(n):
+    # print((2*i + 1) * '*')
+    # print(n-i-1)
+    print('-'*(n-i-1) + (2*i + 1) * '*')
 
-for i in range(5,0,-1):
-    print(i * '*')
+ #     *
+ #    ***
+ #   *****
+ #  *******
+ # *********
