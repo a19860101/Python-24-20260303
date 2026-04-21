23731a34570bd0390de97a504a2b442ffa95680d
Zac <a19860101@gmail.com>
2026-04-21 20:34:42 +0800

    20260421

--- 0421/plot-4.py
+++ 0421/plot-4.py
@@ -1,6 +1,19 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-tsmc = pd.read_csv('./FMSRFK_2330_2025.csv',encoding='big5')
+tsmc = pd.read_csv('./FMSRFK_2330_2025.csv',encoding='big5',header=1)
 
-print(tsmc)
+# print(tsmc)
+# print(tsmc['最高價'])
+
+dataMAX = tsmc['最高價'].dropna().tolist()
+dataMIN = tsmc['最低價'].dropna().tolist()
+
+print(dataMAX)
+print(dataMIN)
+
+x = range(1,13)
+
+plt.plot(x, dataMAX)
+
+plt.show()
