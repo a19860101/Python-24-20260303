e7a61cf1692c5a05eef540a8efaca7c4d0a3ef99
Zac <a19860101@gmail.com>
2026-04-21 19:42:33 +0800

    20260421

--- 0421/plot-3.py
+++ 0421/plot-3.py
@@ -4,6 +4,7 @@ plt.rc('font', family='Microsoft Jhenghei')
 
 dataX = ['日','Mon','Tue','Wed','Thu','Fri','Sat']
 dataY = [28,30,24,10,35,40,16]
+dataY2 = [18,20,14,20,25,30,36]
 
 plt.ylim(0,60)
 # plt.xlim()
@@ -11,6 +12,13 @@ plt.ylim(0,60)
 plt.yticks(range(0,60,5))
 # plt.xticks(range(0,60,5))
 
-plt.plot(dataX,dataY,label='天氣')
+plt.title('weather')
+plt.xlabel('星期')
+plt.ylabel('溫度(攝氏)')
+plt.grid()
+
+plt.plot(dataX,dataY,label='台北一周天氣',marker='o')
+plt.plot(dataX,dataY2,label='東京一周天氣',marker='o')
+
 plt.legend()
 plt.show()
