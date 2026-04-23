e368408c3059d0fa3ac5c59682eeae50c79905df
Zac <a19860101@gmail.com>
2026-04-23 19:12:29 +0800

    20260423

--- 0423/bar-1.py
+++ 0423/bar-1.py
@@ -3,26 +3,27 @@ import matplotlib.pyplot as plt
 plt.rc('font', family='Microsoft Jhenghei')
 
 x = ['A','B','C']
-y = [100,150,80]
+y = [106,148,95]
 
 # 垂直
-# plt.ylim(0,500)
-# plt.yticks(range(0,500,50))
-# plt.grid()
-# plt.xlabel('分類')
-# plt.ylabel('數量')
-# plt.title('垂直長條圖')
-# plt.legend()
-# plt.bar(x, y, label="數量")
+plt.ylim(0,500)
+plt.yticks(range(0,500,50))
+plt.grid()
+plt.xlabel('分類')
+plt.ylabel('數量')
+plt.title('垂直長條圖')
+plt.legend()
+result = plt.bar(x, y, label="數量" , color=['pink','orange','skyblue'], alpha=1, width=0.5, align='center')
+plt.bar_label(result, color='red', fontsize=30)
 
 # 水平
-plt.xlim(0,500)
-plt.xticks(range(0,500,50))
-plt.ylabel('分類')
-plt.xlabel('數量')
-plt.title('水平長條圖')
-plt.barh(x,y,label="數量")
-plt.legend()
+# plt.xlim(0,500)
+# plt.xticks(range(0,500,50))
+# plt.ylabel('分類')
+# plt.xlabel('數量')
+# plt.title('水平長條圖')
+# plt.barh(x,y,label="數量")
+# plt.legend()
 
 
 plt.show()
