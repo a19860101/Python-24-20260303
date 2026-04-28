19ccb2669b24718abf1e2bfcce78b46fd14bca0f
Zac <a19860101@gmail.com>
2026-04-28 21:25:23 +0800

    20260428

--- 0428/scatter-2.py
+++ 0428/scatter-2.py
@@ -6,11 +6,14 @@ np.random.seed(23)
 # 單位:千元
 x = np.random.uniform(10,100,100)
 
+# 銷售數量
 y = 30 + (x * 1.5) + np.random.normal(0,30,100)
 
+# 狀況A：投放少賣很好
 x = np.append(x, [15])
 y = np.append(y,[220])
 
+# 狀況B：投放多賣不好
 x = np.append(x,[92])
 y = np.append(y,[15])
 
