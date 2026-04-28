56b03b0dec06d61ae50c0b4c5bfbca2a4f973a07
Zac <a19860101@gmail.com>
2026-04-28 19:32:50 +0800

    20260428

--- 0428/pie-2.py
+++ 0428/pie-2.py
@@ -3,15 +3,31 @@ import pandas as pd
 
 data = pd.read_csv('./mobile_os.csv')
 
-result = data.iloc[12].tolist()
-result = result[1:]
+# result = data.iloc[12].tolist()
+# result = result[1:]
+# print(data)
+# print(result)
+#
+# label = data.columns[1:]
+# print(label)
+# explode = [0,0.05,0,0]
+#
+# plt.pie(result, labels=label, explode=explode)
+#
+# plt.show()
+
 print(data)
-print(result)
+
+ios = data['iOS'].mean().round(2)
+android = data['Android'].mean().round(2)
+samsung = data['Samsung'].mean().round(2)
+other = data['Other'].mean().round(2)
 
 label = data.columns[1:]
-print(label)
 explode = [0,0.05,0,0]
+os_data = [ios, android, samsung, other]
 
-plt.pie(result, labels=label, explode=explode)
-
+print(os_data)
+plt.pie(os_data, labels=label, explode=explode)
+#
 plt.show()
