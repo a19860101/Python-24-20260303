a84d8c023d4c77db957510118ba0d00505bddc1d
Zac <a19860101@gmail.com>
2026-04-30 20:49:53 +0800

    20260430

--- 0430/s2.py
+++ 0430/s2.py
@@ -5,3 +5,12 @@ data = pd.read_csv('./NPA_TD1.csv', header=1)
 # condition = data['設置縣市'].str.contains('北')
 condition = data['設置縣市'] == '臺北市'
 print(data[condition])
+
+# print(data['設置縣市'].value_counts())
+q = data['設置縣市'].value_counts()
+print(q)
+n = []
+for i in q:
+    n.append(i)
+
+print(n)
