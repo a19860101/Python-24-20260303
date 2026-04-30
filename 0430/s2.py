d423082f5773bae06edfcdd1687f0fa3cc2856aa
Zac <a19860101@gmail.com>
2026-04-30 21:02:14 +0800

    20260430

--- 0430/s2.py
+++ 0430/s2.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+import matplotlib.pyplot as plt
+plt.rcParams['font.sans-serif'] = ['Microsoft Jhenghei']
 data = pd.read_csv('./NPA_TD1.csv', header=1)
 
 # condition = data['設置縣市'].str.contains('北')
@@ -8,9 +9,15 @@ print(data[condition])
 
 # print(data['設置縣市'].value_counts())
 q = data['設置縣市'].value_counts()
-print(q)
-n = []
-for i in q:
-    n.append(i)
 
-print(n)
+# print(q.values)
+# print(q.index)
+q_v = q.values.tolist()
+q_i = q.index.tolist()
+
+print(q_v)
+print(q_i)
+
+# plt.bar(q_i, q_v)
+plt.pie(q_v, labels=q_i)
+plt.show()
