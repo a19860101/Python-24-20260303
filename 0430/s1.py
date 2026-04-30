3b100d0e535591497f82e088e85db7ead1e9a69c
Zac <a19860101@gmail.com>
2026-04-30 19:35:13 +0800

    20260428

--- 0430/s1.py
+++ 0430/s1.py
@@ -1,4 +1,7 @@
 import requests
+import matplotlib.pyplot as plt
+
+plt.rc('font', family='Microsoft Jhenghei')
 
 url = 'https://data.moenv.gov.tw/api/v2/aqx_p_432?api_key=846e44e1-8cc5-4893-ad87-c79d2d383706&limit=1000&sort=ImportDate%20desc&format=JSON'
 
@@ -6,6 +9,25 @@ response = requests.get(url, verify=False)
 
 result = response.json()
 
-for item in result:
-    print(item['sitename'])
-    print(item['aqi'])
+# print(len(result))
+
+# print(result)
+
+top10 = sorted(result, key=lambda x: int(x['aqi']),reverse=True)[:10]
+
+aqi_y = []
+aqi_x = []
+for i in top10:
+    # print(i['sitename'],i['county'],i['aqi'])
+    aqi_y.append(int(i['aqi']))
+    aqi_x.append(i['sitename'])
+
+print(aqi_x)
+print(aqi_y)
+
+plt.yticks(range(0,120,10))
+plt.ylim(0,120)
+plt.grid(axis='y')
+plt.bar(aqi_x,aqi_y)
+
+plt.show()
