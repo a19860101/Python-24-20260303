71011283692eed97e7051290012d43f39583d98d
Zac <a19860101@gmail.com>
2026-05-05 19:13:22 +0800

    20260505

--- 0505/p-1.py
+++ 0505/p-1.py
@@ -1,7 +1,17 @@
+import bs4
 import requests
 
 url = 'https://rate.bot.com.tw/xrt?Lang=zh-TW'
 
 res = requests.get(url)
 
-print(res.text)
+htmlfile = bs4.BeautifulSoup(res.text, 'html.parser')
+
+# print(htmlfile)
+# print(htmlfile.find('h1').text)
+# print(htmlfile.find('p'))
+# print(htmlfile.find('p',class_='text-info').text)
+title = htmlfile.find('h1').text
+
+# print(title)
+print(title.strip())
