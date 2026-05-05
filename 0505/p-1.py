e97db326179e67079f45296a589ec87fe3da33aa
Zac <a19860101@gmail.com>
2026-05-05 19:30:15 +0800

    20260505

--- 0505/p-1.py
+++ 0505/p-1.py
@@ -15,3 +15,12 @@ title = htmlfile.find('h1').text
 
 # print(title)
 print(title.strip())
+
+# print(htmlfile.select_one('tbody tr td:nth-of-type(3)'))
+exc = htmlfile.select('tbody tr td:nth-of-type(3)')
+for e in exc:
+    print(e.text)
+
+exc2 = htmlfile.select('tbody tr td:nth-of-type(5)')
+for e in exc2:
+    print(e.text.strip())
