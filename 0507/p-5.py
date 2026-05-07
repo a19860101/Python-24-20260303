b0b4666c17158ad0c15c3a4247d52795df5758a6
Zac <a19860101@gmail.com>
2026-05-07 19:58:46 +0800

    20260507

--- 0507/p-5.py
+++ 0507/p-5.py
@@ -2,10 +2,15 @@ import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
 
-tsmc = yf.download('2330.TW', period='1y')
+# tsmc = yf.download('2330.TW', period='1y')
+#
+# close = tsmc.get('Close')
+# close.plot()
+# plt.show()
 
-close = tsmc.get('Close')
+tw0050 = yf.download('0050.TW',period='1y')
 
-close.plot()
-
+tw0050[['High','Low']].plot(figsize=(12,6),color=['red','blue'])
+plt.title('0050')
 plt.show()
+
