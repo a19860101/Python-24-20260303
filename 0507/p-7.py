489a913c553690e13c1df57b2cbce0b1678f3cf8
Zac <a19860101@gmail.com>
2026-05-07 21:09:31 +0800

    20260507

--- 0507/p-7.py
+++ 0507/p-7.py
@@ -1,7 +1,22 @@
 import yfinance as yf
 import mplfinance as mpf
+from pygments.lexer import inherit
 
-tsmc = yf.Ticker('2330.TW')
-data = tsmc.history(period="3mo")
+tsmc = yf.Ticker('00899.TW')
+data = tsmc.history(period="6mo")
 
-mpf.plot(data, type='candle')
+# mpf.plot(data, type='candle')
+mc = mpf.make_marketcolors(
+    up='red',
+    down='green',
+    edge='inherit',
+    # volume='in',
+    inherit=True,
+)
+s = mpf.make_mpf_style(
+    base_mpf_style='nightclouds',
+    marketcolors=mc,
+    gridstyle='--'
+)
+
+mpf.plot(data,type='candle',style=s,volume=True,mav=(5,20))
