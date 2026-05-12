5be222ff9ef181d1c65ec0d42e58780fe253f88f
Zac <a19860101@gmail.com>
2026-05-12 19:53:49 +0800

    20260512

--- 0512/802.py
+++ 0512/802.py
@@ -1,2 +1,13 @@
 # 字串轉ASCII
 # ord()
+
+s = input()
+# s = 'Kingdom'
+total = 0
+for i in s:
+    # print(f'ASCII code for \'{i}\' is {ord(i)}')
+    # print(f'ASCII code for "{i}" is {ord(i)}')
+    print(f"ASCII code for '{i}' is {ord(i)}")
+    # total = total + ord(i)
+    total += ord(i)
+print(total)
